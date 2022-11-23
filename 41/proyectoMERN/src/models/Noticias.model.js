b9bd594e2df559ede7e28c969879b454435c95a1
diejofre <diego-jofre@live.com.ar>
2022-11-23 20:11:22 -0300

    change model

--- 41/proyectoMERN/src/models/Noticias.model.js
+++ 41/proyectoMERN/src/models/Noticias.model.js
@@ -4,9 +4,6 @@ const NoticiaSchema = mongoose.Schema({
   titulo: { type: String, required: true },
   cuerpo: { type: String, required: true },
   autor: { type: String, required: true },
-  // imagen: { type: String, required: true },
-  // email: { type: String, required: true },
-  // vista: { type: String, required: true },
 });
 
 const NoticiaModel = mongoose.model("noticias", NoticiaSchema);
